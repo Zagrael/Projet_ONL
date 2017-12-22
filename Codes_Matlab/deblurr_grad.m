418bf23902bcd2b64b67cd37fb1c3a2e752ef80d
Zagrael <aymerick-soyez@outlook.com>
2017-12-22 09:50:13 +0100

    Matlab deblurr_grad

--- Codes_Matlab/deblurr_grad.m
+++ Codes_Matlab/deblurr_grad.m
@@ -7,42 +7,34 @@
 function x = deblurr_grad(A,xtilde,lambda,maxIter)
 
 if ~exist('maxIter','var')
-    maxIter = 300;
+    maxIter = 3000;
 end
 
 n = length(xtilde);
 
-k=1;
-x(:,k) = xtilde;
-x(:,k+1) = zeros(n,1); 
+x(:,1) = xtilde;
+x(:,2) = zeros(n,1); % Second vecteur pour comparer les itérations
+
 iter = 0; % Compteur d'itérations
 
 Q = 2 * ((A' * A) + lambda * eye(n));
 c = -2 * (A' * xtilde);
 
-while (norm(x(:,k) - x(:,k+1),'fro') > 1e-5 && iter <= maxIter)
+while (norm(x(:,1) - x(:,2),'fro') > 1e-10 && iter <= maxIter)
+    % On pose le vecteur de comparaison égal à la précédente itération
+    x(:,2) = x(:,1);
     
-    % Une itération 
+    gf = Q * x(:,1) + c;
+    a = (gf' * gf) / (gf' * Q * gf);
     
-    sym p ;
+    x(:,1) = min(max(x(:,1) - a * gf,0),1);
     
-    x(:,k+1) = x(:,k) - 'p' * (Q * x(:,k) + c);
-    df = Q * x(:,k+1) + c ;
-    
-    eqn = df==0;
-    p1 = solve(eqn, 'p');
-    p1
-    
-    x(:,k+1) = x(:,k) - p1 * (Q * x(:,k) + c);
-    k=k+1;
-    
-    % Avancement
-    
-    clc;
+%     % Visualisation de l'avancement
+%     clc;
     iter = iter + 1;
-    disp('Nombre d''itérations : ');
-    disp(iter);
+%     disp(iter);
     
 end
-x = x(:,k);
+disp(iter);
+x = x(:,1);
 end
